12063350fca666655b61d4bb8a508c301d93c9d9
rice-american <ie.e115703@gmail.com>
2012-07-04 13:35:59 +0900

    ファイル読み込み時404 not foundになる不具合

--- app.js
+++ app.js
@@ -3,20 +3,18 @@ onload = function(){
   init();
 }
 
+var app = require('express').createServer()
+, io = require('socket.io').listen(app)
 function init(){
   // requireして各種サーバを立てる
-  var app = require('express').createServer()
-    , io = require('socket.io').listen(app)
-    , mongo = require('mongodb')
-    , Server = mongo.Server
-    , Db = mongo.Db
-    , roomList = {} // 生成された部屋が格納される連想配列
+  var roomList = {} // 生成された部屋が格納される連想配列
   , standbyQueue = new Array // 待機中のユーザを格納 
-    , members = new Array // roomListのnextkeyのユーザが格納される配列
-    , nextKey = 0; // 次のゲームのkey
+  , members = new Array // roomListのnextkeyのユーザが格納される配列
+  , nextKey = 0; // 次のゲームのkey
 
 
   // connectionイベント
+
   io.sockets.on('connection', function(){
     sockets.on('getStandby', function (socket){
       getStandby(socket);
@@ -54,8 +52,8 @@ function init(){
         });
       }); 
   }
+}
 
-}
 //httpサーバを立てる
 app.listen(8080);    
 //ルートディレクトリにアクセスがあったら、index.htmlへ
@@ -63,25 +61,4 @@ app.get('/', function(req, res) {
   res.sendfile(__dirname + '/views/index.html');
 });
 
-/**************
-  以下、DBコード
- ***************/
-/*
-// DBサーバーオブジェクトを新しく作る   
-var server = new Server('sodadb-RiceAmerican-data-0.dotcloud.com', 30198, {auto_reconnect: true});
-var db = new Db('mydb', server);
 
-db.open(function(err,db){
-if(!err) {
-db.authenticate('dbuser', 'dbpassword', function(err, result){
-db.collection('things', function(err, collection){
-collection.update({"_id":4},{$set:{"value":4}});
-collection.find().toArray(function(err, items) {
-// DBの中身を表示
-//          console.log(items);
-});
-});
-});
-}
-});
-*/
